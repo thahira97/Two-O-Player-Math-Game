0fe749c2d15d285c1e0e628ecfe82614f4f12eb1
thahira97 <thahirasheerin3@gmail.com>
2023-05-02 19:40:57 -0400

    Feat: Add method in question class

--- question.rb
+++ question.rb
@@ -1,5 +1,18 @@
 class Question 
+  attr_accessor :num1, :num2, :answer
+
  def initialize
   @num1 = rand(1..20)
   @num2 = rand(1..20)
+  @answer = @num1 + @num2
+ end
+ 
+ def ques_to_ask
+  puts "What does #{@num1} plus #{@num2} equal?"
+ end
+
+ def check_answer(userinput)
+ @answer = userinput
+ end
+
 end
