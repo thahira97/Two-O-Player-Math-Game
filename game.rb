633b01abee288088617cb179c57cf17267237cac
thahira97 <thahirasheerin3@gmail.com>
2023-05-03 20:11:15 -0400

    Feat: Add Final part of the game

--- game.rb
+++ game.rb
@@ -1,37 +1,55 @@
 class Game
- 
- def initialize
-  @players = [
-    Player.new('Player 1'),
-    Player.new('Player 2')
-  ]
-  @current_player = @players[0]
- end
+  attr_accessor :current_player
 
- def play
-  while @players[0].lives > 0 && @players[1].lives > 0 do
-    question = Question.new()
-    puts "#{@current_player.name}: #{question.ques_to_ask}"
-    player_response = gets.chomp.to_i
-    result = question.check_answer(player_response)
-  end
- end
-
- def validate_answer(res)
-  if res == false
-    @current_player.lives -= 1
-    puts "#{@current_player.name}: Seriously? No!"
-  else
-    puts "#{@current_player.name}: YES! You are correct."
-  end
-  puts "#{@players[0]}: #{@players[0].lives}/3 vs #{@players[1]}: #{@players[1].lives}/3"
- end
-
- def player_switch
-  if @current_player == @players[0]
-    @current_player = @players[1]
-  else
+  def initialize
+    @players = [
+      Player.new("Player 1"),
+      Player.new("Player 2")
+    ]
     @current_player = @players[0]
   end
-end
+
+  def play
+    while @players[0].lives > 0 && @players[1].lives > 0 do
+
+      question = Question.new
+
+      puts "#{@current_player.name}: #{question.ques_to_ask}"
+      print "> "
+      player_response = gets.chomp.to_i
+      result = question.check_answer(player_response)
+      #calling the method to check the answer
+      check_answer(result)
+    end
+    game_lost
+    puts "----- GAME OVER -----"
+    puts "Good bye!"
+  end
+
+  def check_answer(response)
+    if response == false
+      @current_player.lives -= 1
+      puts "#{@current_player.name}: Seriously? No!"
+    else
+      puts "#{@current_player.name}: YES! You are correct."
+    end
+    puts "P1: #{@players[0].lives}/3 vs P2: #{@players[1].lives}/3"
+    switch_player
+    unless @players[0].lives == 0 || @players[1].lives == 0
+      puts "----- NEW TURN -----"
+    end
+  end
+
+  def switch_player
+    if @current_player == @players[0]
+      @current_player = @players[1]
+    else
+      @current_player = @players[0]
+    end
+  end
+
+  def game_lost
+    puts "#{@current_player.name} wins with a score of #{@current_player.lives}/3"
+  end
+
 end
--- question.rb
+++ question.rb
@@ -2,8 +2,8 @@ class Question
   attr_reader :num1, :num2, :answer
 
   def initialize
-    @num1 = rand(0...20)
-    @num2 = rand(0...20)
+    @num1 = rand(1...20)
+    @num2 = rand(1...20)
     @answer = @num1 + @num2
   end
 
